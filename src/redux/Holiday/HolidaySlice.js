a692ce18db605511215d9873b0b1b3d35e9aafac
0sugo <josugo26@gmail.com>
2023-05-23 15:16:29 +0300

    Add slice and createAsyncThunk

---
+++ src/redux/Holiday/HolidaySlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  Holidays: [],
+  isLoading: false,
+};
+
+const url = 'https://date.nager.at/api/v3/AvailableCountries';
+
+export const HolidayFetcher = createAsyncThunk('fetch/Holidays', async () => {
+  try {
+    const response = await axios(url);
+    const resp = response.data;
+    return resp;
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const HolidaySlice = createSlice({
+  name: 'HolidaySlice',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(HolidayFetcher.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(HolidayFetcher.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.Holidays = action.payload;
+        // console.log(action.payload);
+      })
+      .addCase(HolidayFetcher.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export default HolidaySlice.reducer;
