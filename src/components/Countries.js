aec5ff54b579c59fb62340a98f71bccc09bd76dc
0sugo <josugo26@gmail.com>
2023-05-23 23:08:07 +0300

    Add dispatch on click

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,16 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { fetchCountryDetails } from '../redux/InfoCountry/InfoCountrySlice';
 
 const Countries = () => {
   const { AllHolidays } = useSelector((store) => store.allHolidays);
-  // console.log(AllHolidays[0].name);
+
+  const dispatch = useDispatch();
+  // console.log(AllHolidays[0]);
+  const handleDetailsFetch = (e) => {
+    const { id } = e.target;
+    const url = `https://date.nager.at/api/v3/publicholidays/2023/${id}`;
+    // console.log(url);
+    dispatch(fetchCountryDetails({ url }));
+  };
   return (
     <div>
       <p>isLoading...</p>
       <ul>
         {AllHolidays.map((Holiday) => (
           <li key={Holiday.countryCode}>
-            <NavLink to="/CountryDetails">
+            <NavLink to="/CountryDetails" id={Holiday.countryCode} onClick={handleDetailsFetch}>
               {Holiday.name}
             </NavLink>
           </li>
