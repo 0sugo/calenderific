08e4944a17fc1611ba227575167523b22c492394
0sugo <josugo26@gmail.com>
2023-05-23 23:09:44 +0300

    Add components to UI

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const CountryDetails = () => (
-  <div>Specific CountryDetails loading</div>
-);
-
+const CountryDetails = () => {
+  const { country } = useSelector((store) => store.countryDetails);
+  // console.log(country);
+  return (
+    <div>
+      {country.map((item) => (<li key={item.date}>{item.name}</li>))}
+    </div>
+  );
+};
 export default CountryDetails;
