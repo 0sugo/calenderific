2cff98a1b3a0c4327028b45d41a042a3e1ed3778
0sugo <josugo26@gmail.com>
2023-05-25 02:24:09 +0300

    Add country counter functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { LeftChev } from './Icons';
 import globalphoto from './assets/globalphoto.png';
 
-const Navbar = () => (
-  <>
-    <div className="nav-container">
-      <NavLink to="/" className="back"><LeftChev /></NavLink>
-      <h1 className="topper">World Holidays</h1>
-    </div>
-    <div>
-      <img className="header-photo" src={globalphoto} alt="world" />
-    </div>
-  </>
-);
+const Navbar = () => {
+  const { AllHolidays } = useSelector((store) => store.allHolidays);
+
+  return (
+    <>
+      <div className="nav-container">
+        <NavLink to="/" className="back"><LeftChev /></NavLink>
+        <h1 className="topper">World Holidays</h1>
+      </div>
+      <div className="hero-section">
+        <img className="header-photo" src={globalphoto} alt="world" />
+        <div className="All-holidays">
+          <h3>
+            <span className="before-text">Sampled</span>
+            <br />
+            {' '}
+            <span className="main-text">{AllHolidays.length}</span>
+            {' '}
+            <span className="after-text">Countries</span>
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
