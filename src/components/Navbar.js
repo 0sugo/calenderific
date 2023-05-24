cde02bc8618d3d02e3ee3922b19592eecbc0a499
0sugo <josugo26@gmail.com>
2023-05-25 01:35:17 +0300

    Replace angle bracket with left chevron

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import { LeftChev } from './Icons';
+import globalphoto from './assets/globalphoto.png';
 
 const Navbar = () => (
-  <div className="nav-bar">
-    <LeftChev />
-    <h1>World Holidays</h1>
-  </div>
+  <>
+    <div className="nav-container">
+      <NavLink to="/" className="back"><LeftChev /></NavLink>
+      <h1 className="topper">World Holidays</h1>
+    </div>
+    <div>
+      <img className="header-photo" src={globalphoto} alt="world" />
+    </div>
+  </>
 );
 
 export default Navbar;
