d5c823a9cc28db2e8ad5bd683d2fe2380b0ed577
0sugo <josugo26@gmail.com>
2023-05-24 23:53:05 +0300

    Add styles for chev

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -6,7 +6,7 @@ export const RightIcon = () => (
 );
 
 export const LeftChev = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 leftchev">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
   </svg>
 
