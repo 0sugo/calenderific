f7e99956b739e7d0ae938782bb5da2bbb5fa2234
0sugo <josugo26@gmail.com>
2023-05-24 23:50:25 +0300

    Add Navbar to countrydetails

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const CountryDetails = () => {
   const { country } = useSelector((store) => store.countryDetails);
   // console.log(country);
   return (
     <div>
+      <Navbar />
       <NavLink to="/">{'<'}</NavLink>
       <div>
         {country.map((item) => (<div key={item.date}>{item.name}</div>))}
