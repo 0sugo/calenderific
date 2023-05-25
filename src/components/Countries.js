00d1e4a94e6d1f9d77235c84196a621e33130826
0sugo <josugo26@gmail.com>
2023-05-25 15:19:12 +0300

    Add search functionality

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import { fetchCountryDetails } from '../redux/InfoCountry/InfoCountrySlice';
 import { RightIcon } from './Icons';
 import Navbar from './Navbar';
@@ -7,11 +8,19 @@ import Navbar from './Navbar';
 const Countries = () => {
   const { AllHolidays } = useSelector((store) => store.allHolidays);
   const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
   const handleDetailsFetch = (e) => {
     const { id } = e.target;
     const url = `https://date.nager.at/api/v3/publicholidays/2023/${id}`;
     dispatch(fetchCountryDetails({ url }));
   };
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const filteredHoliday = AllHolidays.filter((item) => (
+    item.name.toLowerCase().includes(search.toLowerCase())
+  ));
+
   return (
     <div>
       <div>
@@ -19,13 +28,13 @@ const Countries = () => {
       </div>
       <div className="search-container">
         <p className="search-Title">Search For Country</p>
-        <input className="search-Input-Area" placeholder="e.g. Andorra" />
+        <input className="search-Input-Area" placeholder="e.g. Andorra" onChange={handleSearch} />
       </div>
       {(AllHolidays.isLoading) ? <p>Loading...</p>
 
         : (
           <div className="individual-countries">
-            {AllHolidays.map((Holiday) => (
+            {filteredHoliday.map((Holiday) => (
               <div key={Holiday.countryCode} className="one-country">
                 <RightIcon />
                 <div className="initials">{Holiday.countryCode}</div>
