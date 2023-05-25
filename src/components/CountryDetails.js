afa9c3e76ed2ff9b1f311793422ce4d2df2e25ca
0sugo <josugo26@gmail.com>
2023-05-25 19:05:03 +0300

    Add hero section and specific country holiday count

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
+import globalphoto from './assets/globalphoto.png';
 
 const CountryDetails = () => {
   const { country } = useSelector((store) => store.countryDetails);
-  // console.log(country);
+  const { AllHolidays } = useSelector((store) => store.allHolidays);
+  let filteredCountry = [];
+  let mainText = '0';
+  if (country && country.length > 0) {
+    const id = country[0].countryCode;
+    filteredCountry = AllHolidays.filter((item) => item.countryCode === id);
+    mainText = filteredCountry.length.toString();
+  }
   return (
     <div>
       <Navbar />
+      <div className="hero-section">
+        <img className="header-photo" src={globalphoto} alt="world" />
+        <div className="All-holidays">
+          <h3>
+            <span className="before-text">{filteredCountry.length > 0 ? filteredCountry[0].name : ''}</span>
+            <br />
+            <span className="main-text">{mainText}</span>
+            {' '}
+            <span className="after-text">Holidays</span>
+          </h3>
+        </div>
+      </div>
       <div className="chosen-container">
         {country.map((item) => (
           <div className="chosen-holidays" key={item.date}>
